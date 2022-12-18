b55b86ee96695e2c4e6062a2bab0e84d5bfc4846
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-12-17 21:54:20 -0300

    feat:Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,16 @@ export class AppComponent {
   course = COURSES;
 
 
+  price = 9.99;
+
+  rate = 0.34;
+
+
+  title = COURSES[0].description;
+
+  // PIPE  E UM MECANISMO QUE TRASFORMAS DADOS EM UMA FORMA DE VISUALIZACAO PARA O USUARIO
+  startDate = new Date(2000, 0, 1);
+
   constructor() {
 
   }
