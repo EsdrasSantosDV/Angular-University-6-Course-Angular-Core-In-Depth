aba3edbd529b32efb50abcca6ae7c34b63f08312
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-12-18 20:07:48 -0300

    feat:view Children-Query List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
@@ -18,59 +18,35 @@ export class AppComponent implements AfterViewInit {
   rate = 0.34;
 
 
-  // OBSERVAÇÃO O ESCOPO DO VIEW CHILD E SO PRO TEMPLATE DO COMPONENTE VIGENTE VIGENTE
-  // SE VC TENTAR POR EXEMPLO
-  // PEGAR A REFERENCIA
-  @ViewChild('cardImage')
-  courseImage: ElementRef;
-
-
-  @ViewChild('cardRef1')
-  card: CourseCardComponent;
-
-
-  // PODEMOS PASSAR AS OPOCES EM FORMATO DE OBNJETO
-  // PODEMOS COLOCAR O TIPO DE LEITURA DE REFERENCIA DA NOSSA QUERY DE TEMPLATES
-  // POR DEFAULT VEM A DA INSTANCI A DO COMPONENTE
-  // AQUI COLOCAMOS PRA LER A REFERENCIA DO ELEMENTO DE REFERENCIA
-  @ViewChild('cardRef2', {read: ElementRef})
-  card2: CourseCardComponent;
-
-  // PODEMOS PEGAR  A REFREENCIA NATIVA E TAMBEM DO COMPONENTE SE QUISERMOS
-  // ELEMENTREF E QUALQUER ELEMENTO DO DOOM NATIVO PODEMOS PEGAR O ELEMENTO DE REFERENCIA
-  // DE QUALQUER ELEMENTO, ATE MESMO DOS COMPONENTES. COMO FIZ ALI EMCIMA
-  @ViewChild('container')
-  containerDiv: ElementRef;
-
   title = COURSES[0].description;
 
-  // PIPE  E UM MECANISMO QUE TRASFORMAS DADOS EM UMA FORMA DE VISUALIZACAO PARA O USUARIO
+
+  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  cards: QueryList<ElementRef>;
+
   startDate = new Date(2000, 0, 1);
 
-  // QUANDO AS VARIAVEIS DOS QUERY ELEMENTS TEMPLATES SÃO POPULADOS E QUAL E O MOMENTO
   constructor() {
   }
 
 
   courseSelected($event: Course) {
-    console.log(this.card2);
   }
 
-  // O UNICO MOMENTO E A AFTER VIEW INIT UM DOS CICLOS DE VIDA DO COMPONENTE
+  // O UNICO MOMENTO QUE TEMOS POPULADO A NOSSAS REFERENCIAS E A AFTER VIEW INIT UM DOS CICLOS DE VIDA DO COMPONENTE
   ngAfterViewInit(): void {
 
-    // VAI DARA UNDEFINED PQ O ESCOPO DESSA REFERENCIA E DE OUTRO COMPONENTE ENTÃO NÃO PEGA
-    // NÃO PODEMOS INFLIGIR O A HIERARQUIA DA QUERY DO NOSSO COMPONENTE
-    // O ESCOPO DO VIEW CHILD DECORATOR E SO DO COMPONENTE QUE ELE TA USANDO
-    // ELE E UM LOCAL TEMPLATE QUERY REFERENCE
-    console.log('REFERENCIA DA IMAGEM', this.courseImage);
+    console.log(this.cards);
+  }
 
-    // AQUI E O PONTO QUE TEMOS O VALOR DAS REFERENCIAS
-
-    // ISSO VAI DAR ERRO
-    // PORUQE ESTAMOS TENTANDO MOSTRAR O DATA CONSTRUDIA PELA VIEW E ESTAMOS
-    // O VLAOR CONSTRUIDO PELA VIEW
-    // this.course[0].description = 'test';
-
+  onCoursesEdited() {
+    this.course.push({
+      id: 1,
+      description: 'Angular Core Deep Dive',
+      iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+      longDescription: 'A detailed walk-through of the most important part of Angular - the Core and Common modules',
+      category: 'INTERMEDIATE',
+      lessonsCount: 10
+    });
   }
 }
