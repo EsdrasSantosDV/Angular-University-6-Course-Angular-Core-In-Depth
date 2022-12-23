d474e99f720e0c9f04f72fc21fbd7acac88ea395
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-12-23 15:39:53 -0300

    feat:directive structural

---
+++ src/app/directives/ngx-unless.directive.ts
@@ -0,0 +1,31 @@
+import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
+
+@Directive({
+  selector: '[ngxUnless]'
+})
+
+// E BOA PRATICA COLOCAR O X PRA NÃO CONFUDIR COM AS DIRETIVAS NATIVAS DO CORE DO ANGULAR
+export class NgxUnlessDirective {
+
+  visible = false;
+
+
+  constructor(private templateRef: TemplateRef<any>,
+              private viewContainer: ViewContainerRef) {
+
+
+  }
+
+  @Input()
+  set ngxUnless(condition: boolean) {
+    if (!condition && !this.visible) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.visible = true;
+    } else if (condition && this.visible) {
+      this.viewContainer.clear();
+      this.visible = false;
+    }
+
+  }
+
+}
