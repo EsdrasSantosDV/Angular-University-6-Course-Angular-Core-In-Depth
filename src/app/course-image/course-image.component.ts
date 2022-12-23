213d53ab11f6188f7a29203508fb97acd77c11d1
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-12-22 21:27:21 -0300

    feat:content children e content child query

---
+++ src/app/course-image/course-image.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'course-image',
+  templateUrl: './course-image.component.html',
+  styleUrls: ['./course-image.component.css']
+})
+export class CourseImageComponent {
+
+  @Input('src')
+  imgUrl: string;
+
+
+}
