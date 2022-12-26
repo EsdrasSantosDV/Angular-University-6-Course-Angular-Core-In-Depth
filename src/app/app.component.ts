4477bfeb2b2a25c493882a2b5ce642a6418b8903
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-12-26 14:03:17 -0300

    feat:http client params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {logging} from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +15,22 @@ import {Observable} from 'rxjs';
 export class AppComponent implements OnInit {
 
 
-  courses = COURSES;
+  courses;
 
-  constructor() {
+  constructor(private http: HttpClient) {
 
   }
 
+
+  // Melhor lugar pra colocar a logica de inicializaççao, trigering e a chamadas do backend
   ngOnInit() {
+
+    const params = new HttpParams().set('page', '1').set('pageSize', '10');
+
+
+
+    // QUANDO O ANGULAR INSTANCEIA O COMPONENTE, A PRIMEIRA COISA QUE CHAMA E O CONSTRUCTOR E PASSA AS DEPENDENCIAS MULTIPLAS
+    this.http.get('/api/courses', {params}).subscribe(courses => this.courses = courses);
   }
 
 
