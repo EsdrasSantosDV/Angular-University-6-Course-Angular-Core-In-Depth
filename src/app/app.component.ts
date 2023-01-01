1d1087166538f48a5a74ae05529ef9693cd53bdc
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-01-01 19:49:07 -0300

    feat:Optional,Self,SkipSelf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {
   Component,
   ElementRef, Inject,
   InjectionToken,
-  OnInit,
+  OnInit, Optional,
   QueryList,
   ViewChild,
   ViewChildren
@@ -23,14 +23,20 @@ import {AppConfig, CONFIG_TOKEN} from './config';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[CoursesService]
   // USANDO O PROVEDOR DE THREE SHAKEL NÃO PRECISAMOS COLOCAR AQUI O PROVIDER
 })
 export class AppComponent implements OnInit {
 
   courses$: Observable<Course[]>;
+/*
+Portanto, o decorador opcional destina-se a situações em que não temos certeza se uma dependência será ser fornecido.
+Dessa forma, podemos marcar a dependência como opcional e então temos que tratar programaticamente os casos
+onde a dependência é indefinida.Em nosso código.
+ */
 
-  constructor( private courseS: CoursesService, @Inject(CONFIG_TOKEN)private config: AppConfig) {
+  constructor(@Optional() private courseS: CoursesService, @Inject(CONFIG_TOKEN)private config: AppConfig) {
     console.log(this.config.apiUrl);
   }
 
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,15 +1,15 @@
 import {
-    AfterContentInit,
-    AfterViewInit,
-    Component,
-    ContentChildren,
-    ElementRef,
-    EventEmitter,
-    Input,
-    OnInit,
-    Output,
-    QueryList,
-    ViewEncapsulation
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChildren,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  QueryList, Self, SkipSelf,
+  ViewEncapsulation
 } from '@angular/core';
 import {Course} from '../model/course';
 import {CourseImageComponent} from '../course-image/course-image.component';
@@ -18,27 +18,9 @@ import {CoursesService} from '../services/courses.service';
 @Component({
     selector: 'course-card',
     templateUrl: './course-card.component.html',
-    styleUrls: ['./course-card.component.css']
-  // SE NÃO COLOCAMOS MANUALMENTE AQUI A INJEÇÃO DO COURSES SERVICES
-  // ESSE COMPONENTE VAI TENTAR ACHAR UM PROVEDOR PRA DEPENDECIA DESSE SERVIÇO LA NO CONSTRUCTOR
-  // SE ELE NÃO ENCONTRAR
-  // ELE VAI PEDIR PRO COMPONENTE PAI DESSE COMPOENNTE AQUI
-  // UM PROVEDOR QUE SATISFAÇA ESSA DEPEDENCIA
-  // E SE FOR O CASO
-  // ENTÃO O COMPONENTE PAI IRA DAR PRO SEU FILHO A DEPENDICA NECESSARIA
-  // PÓR ISSO CHAMOS O SISTEMA DE DEPENCIA HIERARQUICO
-  // E POR QUE SEGUE UMA HIERARAQUE DE INJECAO DE DEPENDENCIA DOS COMPONENTES
-  // CASO O COMPONENTE NÃO POSSA CRIAR A DEPEDENCIA POR CONTA PROPRIA
-  // ISSO VAI PERCORRER TODO O CAMINHO
-  // ATE A RAIZ DO APP
-  // ISSO SIGNIFICA QUE O SISTEMA DE INJECAO DE DEEPENCIA DO ANGULAR PERMITA
-  // QUE CADA COMPONENTE CRIE SUAS PROPRIAS DEPENEDENCIAS SE FOR NECESSARIO
-  // CASO O SERVICÇO QUE ESTAMOS USANDO UTILZIE UM ESTADO QUE SEJA PRIVADO PRA
-  // DETERMINADA PARTE DO APP
-  // PODEMOS PROVER ELE SO A NIVEL DO COMPONENTE
-  // OU SE ELE PODE SER REUTILIZADO USAMOS OS TRES PROVEDORES DE SHAKAL
+    styleUrls: ['./course-card.component.css'],
+    providers:[CoursesService]
 
-  // providers:[CoursesService]
 })
 export class CourseCardComponent implements OnInit {
 
@@ -51,8 +33,16 @@ export class CourseCardComponent implements OnInit {
     @Output('courseChanged')
     courseEmitter = new EventEmitter<Course>();
 
+  /*
+    SELF DECORATOR, A INJECAO VAI USAR O SERVIÇO DO ESCOPO DELE E NÃO DO SISTEMA DE HIERARQUIA DO ANGUALAR
+    //ONDE ELE HERDA DO PAI
+     */
 
-    constructor(private courseS: CoursesService) {
+  /*
+  SKIP SELF DECORATOR, A INJEÇÃO VAI IGNORAR O SERVIÇO DO ESCOPO DO COMPONENTE, E VAI INJETAR O DO COMPONENTE PAI
+   */
+
+    constructor(@Self() private courseS: CoursesService) {
       console.log(this.courseS);
     }
 
